a488ab6c93af3f0f7e1ca8f04321f460eba0e823
Bartek <bartosz.baczek@gmail.com>
2015-10-14 09:56:28 +0200

    Added new form for setting order of elements in DataGridView

---
+++ WelderCalculator/WelderCalculator/MaterialDatabasePropertiesView/MaterialDatabasePropertiesForm.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace WelderCalculator.MaterialDatabasePropertiesView
+{
+    partial class MaterialDatabasePropertiesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1st = new System.Windows.Forms.ComboBox();
+            this.comboBox2nd = new System.Windows.Forms.ComboBox();
+            this.comboBox3th = new System.Windows.Forms.ComboBox();
+            this.comboBox4th = new System.Windows.Forms.ComboBox();
+            this.comboBox5th = new System.Windows.Forms.ComboBox();
+            this.comboBox6th = new System.Windows.Forms.ComboBox();
+            this.comboBox7th = new System.Windows.Forms.ComboBox();
+            this.comboBox8th = new System.Windows.Forms.ComboBox();
+            this.comboBox9th = new System.Windows.Forms.ComboBox();
+            this.comboBox10th = new System.Windows.Forms.ComboBox();
+            this.comboBox11th = new System.Windows.Forms.ComboBox();
+            this.comboBox12th = new System.Windows.Forms.ComboBox();
+            this.comboBox13th = new System.Windows.Forms.ComboBox();
+            this.comboBox14th = new System.Windows.Forms.ComboBox();
+            this.label1st = new System.Windows.Forms.Label();
+            this.label2nd = new System.Windows.Forms.Label();
+            this.label4th = new System.Windows.Forms.Label();
+            this.label3rd = new System.Windows.Forms.Label();
+            this.label8th = new System.Windows.Forms.Label();
+            this.label7th = new System.Windows.Forms.Label();
+            this.label6th = new System.Windows.Forms.Label();
+            this.label5th = new System.Windows.Forms.Label();
+            this.label12th = new System.Windows.Forms.Label();
+            this.label11th = new System.Windows.Forms.Label();
+            this.label10th = new System.Windows.Forms.Label();
+            this.label9th = new System.Windows.Forms.Label();
+            this.label14th = new System.Windows.Forms.Label();
+            this.label13th = new System.Windows.Forms.Label();
+            this.OrderLayoutPanel = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1st
+            // 
+            this.comboBox1st.FormattingEnabled = true;
+            this.comboBox1st.Location = new System.Drawing.Point(40, 41);
+            this.comboBox1st.Name = "comboBox1st";
+            this.comboBox1st.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1st.TabIndex = 0;
+            // 
+            // comboBox2nd
+            // 
+            this.comboBox2nd.FormattingEnabled = true;
+            this.comboBox2nd.Location = new System.Drawing.Point(40, 68);
+            this.comboBox2nd.Name = "comboBox2nd";
+            this.comboBox2nd.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2nd.TabIndex = 1;
+            // 
+            // comboBox3th
+            // 
+            this.comboBox3th.FormattingEnabled = true;
+            this.comboBox3th.Location = new System.Drawing.Point(40, 95);
+            this.comboBox3th.Name = "comboBox3th";
+            this.comboBox3th.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3th.TabIndex = 3;
+            // 
+            // comboBox4th
+            // 
+            this.comboBox4th.FormattingEnabled = true;
+            this.comboBox4th.Location = new System.Drawing.Point(40, 122);
+            this.comboBox4th.Name = "comboBox4th";
+            this.comboBox4th.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4th.TabIndex = 2;
+            // 
+            // comboBox5th
+            // 
+            this.comboBox5th.FormattingEnabled = true;
+            this.comboBox5th.Location = new System.Drawing.Point(40, 149);
+            this.comboBox5th.Name = "comboBox5th";
+            this.comboBox5th.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5th.TabIndex = 5;
+            // 
+            // comboBox6th
+            // 
+            this.comboBox6th.FormattingEnabled = true;
+            this.comboBox6th.Location = new System.Drawing.Point(40, 176);
+            this.comboBox6th.Name = "comboBox6th";
+            this.comboBox6th.Size = new System.Drawing.Size(121, 21);
+            this.comboBox6th.TabIndex = 4;
+            // 
+            // comboBox7th
+            // 
+            this.comboBox7th.FormattingEnabled = true;
+            this.comboBox7th.Location = new System.Drawing.Point(40, 203);
+            this.comboBox7th.Name = "comboBox7th";
+            this.comboBox7th.Size = new System.Drawing.Size(121, 21);
+            this.comboBox7th.TabIndex = 11;
+            // 
+            // comboBox8th
+            // 
+            this.comboBox8th.FormattingEnabled = true;
+            this.comboBox8th.Location = new System.Drawing.Point(40, 230);
+            this.comboBox8th.Name = "comboBox8th";
+            this.comboBox8th.Size = new System.Drawing.Size(121, 21);
+            this.comboBox8th.TabIndex = 10;
+            // 
+            // comboBox9th
+            // 
+            this.comboBox9th.FormattingEnabled = true;
+            this.comboBox9th.Location = new System.Drawing.Point(40, 257);
+            this.comboBox9th.Name = "comboBox9th";
+            this.comboBox9th.Size = new System.Drawing.Size(121, 21);
+            this.comboBox9th.TabIndex = 9;
+            // 
+            // comboBox10th
+            // 
+            this.comboBox10th.FormattingEnabled = true;
+            this.comboBox10th.Location = new System.Drawing.Point(40, 284);
+            this.comboBox10th.Name = "comboBox10th";
+            this.comboBox10th.Size = new System.Drawing.Size(121, 21);
+            this.comboBox10th.TabIndex = 8;
+            // 
+            // comboBox11th
+            // 
+            this.comboBox11th.FormattingEnabled = true;
+            this.comboBox11th.Location = new System.Drawing.Point(40, 311);
+            this.comboBox11th.Name = "comboBox11th";
+            this.comboBox11th.Size = new System.Drawing.Size(121, 21);
+            this.comboBox11th.TabIndex = 7;
+            // 
+            // comboBox12th
+            // 
+            this.comboBox12th.FormattingEnabled = true;
+            this.comboBox12th.Location = new System.Drawing.Point(40, 338);
+            this.comboBox12th.Name = "comboBox12th";
+            this.comboBox12th.Size = new System.Drawing.Size(121, 21);
+            this.comboBox12th.TabIndex = 6;
+            // 
+            // comboBox13th
+            // 
+            this.comboBox13th.FormattingEnabled = true;
+            this.comboBox13th.Location = new System.Drawing.Point(40, 365);
+            this.comboBox13th.Name = "comboBox13th";
+            this.comboBox13th.Size = new System.Drawing.Size(121, 21);
+            this.comboBox13th.TabIndex = 13;
+            // 
+            // comboBox14th
+            // 
+            this.comboBox14th.FormattingEnabled = true;
+            this.comboBox14th.Location = new System.Drawing.Point(40, 392);
+            this.comboBox14th.Name = "comboBox14th";
+            this.comboBox14th.Size = new System.Drawing.Size(121, 21);
+            this.comboBox14th.TabIndex = 12;
+            // 
+            // label1st
+            // 
+            this.label1st.AutoSize = true;
+            this.label1st.Location = new System.Drawing.Point(18, 44);
+            this.label1st.Name = "label1st";
+            this.label1st.Size = new System.Drawing.Size(16, 13);
+            this.label1st.TabIndex = 14;
+            this.label1st.Text = "1.";
+            // 
+            // label2nd
+            // 
+            this.label2nd.AutoSize = true;
+            this.label2nd.Location = new System.Drawing.Point(18, 71);
+            this.label2nd.Name = "label2nd";
+            this.label2nd.Size = new System.Drawing.Size(16, 13);
+            this.label2nd.TabIndex = 15;
+            this.label2nd.Text = "2.";
+            // 
+            // label4th
+            // 
+            this.label4th.AutoSize = true;
+            this.label4th.Location = new System.Drawing.Point(18, 125);
+            this.label4th.Name = "label4th";
+            this.label4th.Size = new System.Drawing.Size(16, 13);
+            this.label4th.TabIndex = 17;
+            this.label4th.Text = "4.";
+            // 
+            // label3rd
+            // 
+            this.label3rd.AutoSize = true;
+            this.label3rd.Location = new System.Drawing.Point(18, 98);
+            this.label3rd.Name = "label3rd";
+            this.label3rd.Size = new System.Drawing.Size(16, 13);
+            this.label3rd.TabIndex = 16;
+            this.label3rd.Text = "3.";
+            // 
+            // label8th
+            // 
+            this.label8th.AutoSize = true;
+            this.label8th.Location = new System.Drawing.Point(18, 233);
+            this.label8th.Name = "label8th";
+            this.label8th.Size = new System.Drawing.Size(16, 13);
+            this.label8th.TabIndex = 21;
+            this.label8th.Text = "8.";
+            // 
+            // label7th
+            // 
+            this.label7th.AutoSize = true;
+            this.label7th.Location = new System.Drawing.Point(18, 206);
+            this.label7th.Name = "label7th";
+            this.label7th.Size = new System.Drawing.Size(16, 13);
+            this.label7th.TabIndex = 20;
+            this.label7th.Text = "7.";
+            // 
+            // label6th
+            // 
+            this.label6th.AutoSize = true;
+            this.label6th.Location = new System.Drawing.Point(18, 179);
+            this.label6th.Name = "label6th";
+            this.label6th.Size = new System.Drawing.Size(16, 13);
+            this.label6th.TabIndex = 19;
+            this.label6th.Text = "6.";
+            // 
+            // label5th
+            // 
+            this.label5th.AutoSize = true;
+            this.label5th.Location = new System.Drawing.Point(18, 152);
+            this.label5th.Name = "label5th";
+            this.label5th.Size = new System.Drawing.Size(16, 13);
+            this.label5th.TabIndex = 18;
+            this.label5th.Text = "5.";
+            // 
+            // label12th
+            // 
+            this.label12th.AutoSize = true;
+            this.label12th.Location = new System.Drawing.Point(12, 341);
+            this.label12th.Name = "label12th";
+            this.label12th.Size = new System.Drawing.Size(22, 13);
+            this.label12th.TabIndex = 25;
+            this.label12th.Text = "12.";
+            // 
+            // label11th
+            // 
+            this.label11th.AutoSize = true;
+            this.label11th.Location = new System.Drawing.Point(12, 314);
+            this.label11th.Name = "label11th";
+            this.label11th.Size = new System.Drawing.Size(22, 13);
+            this.label11th.TabIndex = 24;
+            this.label11th.Text = "11.";
+            // 
+            // label10th
+            // 
+            this.label10th.AutoSize = true;
+            this.label10th.Location = new System.Drawing.Point(12, 287);
+            this.label10th.Name = "label10th";
+            this.label10th.Size = new System.Drawing.Size(22, 13);
+            this.label10th.TabIndex = 23;
+            this.label10th.Text = "10.";
+            // 
+            // label9th
+            // 
+            this.label9th.AutoSize = true;
+            this.label9th.Location = new System.Drawing.Point(18, 260);
+            this.label9th.Name = "label9th";
+            this.label9th.Size = new System.Drawing.Size(16, 13);
+            this.label9th.TabIndex = 22;
+            this.label9th.Text = "9.";
+            // 
+            // label14th
+            // 
+            this.label14th.AutoSize = true;
+            this.label14th.Location = new System.Drawing.Point(12, 395);
+            this.label14th.Name = "label14th";
+            this.label14th.Size = new System.Drawing.Size(22, 13);
+            this.label14th.TabIndex = 27;
+            this.label14th.Text = "14.";
+            // 
+            // label13th
+            // 
+            this.label13th.AutoSize = true;
+            this.label13th.Location = new System.Drawing.Point(12, 368);
+            this.label13th.Name = "label13th";
+            this.label13th.Size = new System.Drawing.Size(22, 13);
+            this.label13th.TabIndex = 26;
+            this.label13th.Text = "13.";
+            // 
+            // OrderLayoutPanel
+            // 
+            this.OrderLayoutPanel.Location = new System.Drawing.Point(12, 14);
+            this.OrderLayoutPanel.Name = "OrderLayoutPanel";
+            this.OrderLayoutPanel.Size = new System.Drawing.Size(159, 410);
+            this.OrderLayoutPanel.TabIndex = 28;
+            this.OrderLayoutPanel.TabStop = false;
+            this.OrderLayoutPanel.Text = "Kolejność";
+            // 
+            // MaterialDatabasePropertiesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(201, 469);
+            this.Controls.Add(this.label14th);
+            this.Controls.Add(this.label13th);
+            this.Controls.Add(this.label12th);
+            this.Controls.Add(this.label11th);
+            this.Controls.Add(this.label10th);
+            this.Controls.Add(this.label9th);
+            this.Controls.Add(this.label8th);
+            this.Controls.Add(this.label7th);
+            this.Controls.Add(this.label6th);
+            this.Controls.Add(this.label5th);
+            this.Controls.Add(this.label4th);
+            this.Controls.Add(this.label3rd);
+            this.Controls.Add(this.label2nd);
+            this.Controls.Add(this.label1st);
+            this.Controls.Add(this.comboBox13th);
+            this.Controls.Add(this.comboBox14th);
+            this.Controls.Add(this.comboBox7th);
+            this.Controls.Add(this.comboBox8th);
+            this.Controls.Add(this.comboBox9th);
+            this.Controls.Add(this.comboBox10th);
+            this.Controls.Add(this.comboBox11th);
+            this.Controls.Add(this.comboBox12th);
+            this.Controls.Add(this.comboBox5th);
+            this.Controls.Add(this.comboBox6th);
+            this.Controls.Add(this.comboBox3th);
+            this.Controls.Add(this.comboBox4th);
+            this.Controls.Add(this.comboBox2nd);
+            this.Controls.Add(this.comboBox1st);
+            this.Controls.Add(this.OrderLayoutPanel);
+            this.Name = "MaterialDatabasePropertiesForm";
+            this.Text = "OrderOfElementProperties";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1st;
+        private System.Windows.Forms.ComboBox comboBox2nd;
+        private System.Windows.Forms.ComboBox comboBox3th;
+        private System.Windows.Forms.ComboBox comboBox4th;
+        private System.Windows.Forms.ComboBox comboBox5th;
+        private System.Windows.Forms.ComboBox comboBox6th;
+        private System.Windows.Forms.ComboBox comboBox7th;
+        private System.Windows.Forms.ComboBox comboBox8th;
+        private System.Windows.Forms.ComboBox comboBox9th;
+        private System.Windows.Forms.ComboBox comboBox10th;
+        private System.Windows.Forms.ComboBox comboBox11th;
+        private System.Windows.Forms.ComboBox comboBox12th;
+        private System.Windows.Forms.ComboBox comboBox13th;
+        private System.Windows.Forms.ComboBox comboBox14th;
+        private System.Windows.Forms.Label label1st;
+        private System.Windows.Forms.Label label2nd;
+        private System.Windows.Forms.Label label4th;
+        private System.Windows.Forms.Label label3rd;
+        private System.Windows.Forms.Label label8th;
+        private System.Windows.Forms.Label label7th;
+        private System.Windows.Forms.Label label6th;
+        private System.Windows.Forms.Label label5th;
+        private System.Windows.Forms.Label label12th;
+        private System.Windows.Forms.Label label11th;
+        private System.Windows.Forms.Label label10th;
+        private System.Windows.Forms.Label label9th;
+        private System.Windows.Forms.Label label14th;
+        private System.Windows.Forms.Label label13th;
+        private System.Windows.Forms.GroupBox OrderLayoutPanel;
+
+    }
+}
