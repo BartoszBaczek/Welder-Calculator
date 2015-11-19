30c3634dede20065bac8ddecc781135b8e12b49c
Bartek <bartosz.baczek@gmail.com>
2015-11-19 11:00:36 +0100

    ehhh

--- WelderCalculator/WelderCalculator/MaterialModificationView/MaterialModificationPresenter.cs
+++ WelderCalculator/WelderCalculator/MaterialModificationView/MaterialModificationPresenter.cs
@@ -33,6 +33,35 @@ namespace WelderCalculator.MaterialModificationView
             _view.NumberTextbox = material.Number;
             _view.GuidTextbox = material.GuidNumber.ToString();
 
+            Element e = material.GetElement(Category.OfElement.C);
+            _view.CMintextbox = e.Min;
+            _view.CMaxtextbox = e.Max;
+            _view.CRealtextbox = e.RealValue;
+
+            e = material.GetElement(Category.OfElement.Si);
+            _view.SiMintextbox = e.Min;
+            _view.SiMaxtextbox = e.Max;
+            _view.SiRealtextbox = e.RealValue;
+
+            e = material.GetElement(Category.OfElement.Mn);
+            _view.MnMintextbox = e.Min;
+            _view.MnMaxtextbox = e.Max;
+            _view.MnRealtextbox = e.RealValue;
+
+            e = material.GetElement(Category.OfElement.P);
+            _view.PMintextbox = e.Min;
+            _view.PMaxtextbox = e.Max;
+            _view.PRealtextbox = e.RealValue;
+
+            e = material.GetElement(Category.OfElement.S);
+            _view.SMintextbox = e.Min;
+            _view.SMaxtextbox = e.Max;
+            _view.SRealtextbox = e.RealValue;
+
+            e = material.GetElement(Category.OfElement.N);
+            _view.NMintextbox = e.Min;
+            _view.NMaxtextbox = e.Max;
+            _view.NRealtextbox = e.RealValue;
         }
     }
 }
