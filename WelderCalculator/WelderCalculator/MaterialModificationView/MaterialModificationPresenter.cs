439730f91c824afc8ae1d13ff04f50dc694a2f13
Bartek <bartosz.baczek@gmail.com>
2015-11-19 08:32:40 +0100

    shit, i forgot that element data contains min, max and real - need to do improve form

--- WelderCalculator/WelderCalculator/MaterialModificationView/MaterialModificationPresenter.cs
+++ WelderCalculator/WelderCalculator/MaterialModificationView/MaterialModificationPresenter.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WelderCalculator.Model;
 
 namespace WelderCalculator.MaterialModificationView
 {
     public class MaterialModificationPresenter
     {
+        private readonly IMaterialModificationView _view;
+        //connector to repo?
+
+        public MaterialModificationPresenter(IMaterialModificationView view)
+        {
+            _view = view;
+            view.Presenter = this;
+            //_connector = new connector?
+        }
+
+        public MaterialModificationPresenter(IMaterialModificationView view, Material materialToModify)
+        {
+            _view = view;
+            view.Presenter = this;
+            //_connector = new connector?
+            BindToControls(materialToModify);
+        }
+
+        private void BindToControls(Material material)
+        {
+            _view.NameTextbox = material.Name;
+            _view.NumberTextbox = material.Number;
+            _view.GuidTextbox = material.GuidNumber.ToString();
+
+            _view.Ctextbox = material.GetElement(Category.OfElement.C);
+
+        }
     }
 }
