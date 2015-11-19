8d6f0e010ab35841ad99404e0baa439d1c778faf
Bartek <bartosz.baczek@gmail.com>
2015-11-19 08:05:56 +0100

    Presenter accesibility to public & IMaterialModificationView done

--- WelderCalculator/WelderCalculator/MaterialModificationView/IMaterialModificationView.cs
+++ WelderCalculator/WelderCalculator/MaterialModificationView/IMaterialModificationView.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace WelderCalculator.MaterialModificationView
 {
-    interface IMaterialModificationView
+    public interface IMaterialModificationView
     {
+        MaterialModificationPresenter Presenter { set; }
+
+        string NameTextbox { get; set; }
+        string NumberTextbox { get; set; }
+        string GuidTextbox { get; set; }
+
+        double? Ctextbox { get; set; }
+        double? Sitextbox { get; set; }
+        double? Mntextbox { get; set; }
+        double? Ptextbox { get; set; }
+        double? Stextbox { get; set; }
+        double? Ntextbox { get; set; }
+        double? Crtextbox { get; set; }
+        double? Motextbox { get; set; }
+        double? Nbtextbox { get; set; }
+        double? Nitextbox { get; set; }
+        double? Titextbox { get; set; }
+        double? Altextbox { get; set; }
+        double? Vtextbox { get; set; }
+        double? Cutextbox { get; set; }
     }
 }
--- WelderCalculator/WelderCalculator/MaterialModificationView/MaterialModificationPresenter.cs
+++ WelderCalculator/WelderCalculator/MaterialModificationView/MaterialModificationPresenter.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WelderCalculator.MaterialModificationView
 {
-    class MaterialModificationPresenter
+    public class MaterialModificationPresenter
     {
     }
 }
