65bb4d4632b3a85c9e1ea4baa23298e1063d3134
Bartek <bartosz.baczek@gmail.com>
2016-04-26 09:56:21 +0200

    Moving charts size data to separate file

---
+++ WelderCalculator/WelderCalculator/Model/ChartSizing.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace WelderCalculator.Model
+{
+    public class ChartSizing
+    {
+        public PointF _imageWidthAndHeight { get; set; }
+        public PointF _OriginXAndY { get; set; }
+    }
+}
