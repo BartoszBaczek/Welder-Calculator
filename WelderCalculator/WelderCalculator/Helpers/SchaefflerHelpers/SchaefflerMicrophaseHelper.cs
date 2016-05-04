44c975fc67f1717fdbabf5dba836023094939efd
Bartek <bartosz.baczek@gmail.com>
2016-05-04 09:55:13 +0200

    added class checking if point is in given microphase area on schaeffler diagram

---
+++ WelderCalculator/WelderCalculator/Helpers/SchaefflerHelpers/SchaefflerMicrophaseHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelderCalculator.Helpers.SchaefflerHelpers
+{
+    public class SchaefflerMicrophaseHelper
+    {
+        // ferrite, martensite, austenite
+        public bool FMContainst(PointF point)
+        {
+            return false;
+        }
+
+        public bool MContains(PointF point)
+        {
+            return false;
+        }
+
+        public bool AMContains(PointF point)
+        {
+            return false;
+        }
+
+        public bool AContains(PointF point)
+        {
+            return false;
+        }
+
+        public bool MFContains(PointF point)
+        {
+            return false;
+        }
+
+        public bool FContains(PointF point)
+        {
+            return false;
+        }
+
+        public bool AMFContains(PointF point)
+        {
+            return false;
+        }
+
+        public bool F0to5Contains(PointF point)
+        {
+            return false;
+        }
+
+        public bool F5to10Contains(PointF point)
+        {
+            return false;
+        }
+
+        public bool F10to20Contains(PointF point)
+        {
+            return false;
+        }
+
+        public bool F20to40Contains(PointF point)
+        {
+            return false;
+        }
+
+        public bool F40to80Contains(PointF point)
+        {
+            return false;
+        }
+
+        public bool F80to100Contains(PointF point)
+        {
+            return false;
+        }
+    }
+}
