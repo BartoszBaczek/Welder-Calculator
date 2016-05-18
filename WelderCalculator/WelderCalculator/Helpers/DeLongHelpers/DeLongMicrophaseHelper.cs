2bd92600799f22b96bbd78adc5deadbaceea96ea
bartoszbaczek <bartosz.baczek@gmail.com>
2016-05-18 15:03:33 +0200

    Imlemented few methods in DeLongMicrophaseHelper

--- WelderCalculator/WelderCalculator/Helpers/DeLongHelpers/DeLongMicrophaseHelper.cs
+++ WelderCalculator/WelderCalculator/Helpers/DeLongHelpers/DeLongMicrophaseHelper.cs
@@ -2,64 +2,59 @@
 {
     public class DeLongMicrophaseHelper
     {
-        private float yInAmArea(float x, float y)
+        private float yInAmBorder(float x)
         {
-            return 0;
+            return (-46f / 55f) * x - (1539f / 55f);
         }
 
-        private float yInAArea(float x, float y)
+        private float yIn0to2FBorder(float x)
         {
-            return 0;
+            return (104f / 75f) * x - (1813f / 150f);
         }
 
-        private float yIn0to2FArea(float x, float y)
+        private float yIn2t42FBorder(float x)
         {
-            return 0;
+            return (110f / 79f) * x - (1036f / 79f);
         }
 
-        private float yIn2t42FArea(float x, float y)
+        private float yIn4to6FBorder(float x)
         {
-            return 0;
+            return (110f / 79f) * x - (1102f / 79f);
         }
 
-        private float yIn4to6FArea(float x, float y)
+        private float yIn6to8FBorder(float x)
         {
-            return 0;
+            return (7f / 5f) * x - (149f / 10f);
         }
 
-        private float yIn6to8FArea(float x, float y)
+        private float yIn8to10FBorder(float x)
         {
-            return 0;
+            return (100f / 73f) * x - (1090f / 73f);
         }
 
-        private float yIn8to10FArea(float x, float y)
+        private float yIn10to12Border(float x)
         {
-            return 0;
+            return (4f / 3f) * x - (44f / 3f);
         }
 
-        private float yIn10to12FArea(float x, float y)
+        private float yIn12to14FBorder(float x)
         {
-            return 0;
+            return (99f / 76f) * x - (11111f / 760f);
         }
 
-        private float yIn12to14FArea(float x, float y)
+        private float yIn14to16FBorder(float x)
         {
-            return 0;
+            return (9f / 7f) * x - (522f / 35f);
         }
 
-        private float yIn14to16FArea(float x, float y)
+        private float yIn16to18FBorder(float x)
         {
-            return 0;
+            return (72f / 55f) * x - (888f / 55f);
         }
 
-        private float yIn16to18FArea(float x, float y)
+        private float yIn18PlusFBorder(float x)
         {
-            return 0;
-        }
-
-        private float yIn18PlusFArea(float x, float y)
-        {
-            return 0;
+            return (84f / 67f) * x - (5176f / 335f);
         }
     }
 }
