e0d40551aa84deb34aa8845f7c4c3f2095dbd8e9
bartoszbaczek <bartosz.baczek@gmail.com>
2016-05-20 16:08:41 +0200

    added 2 methods to delongMicroophaseHelper

--- WelderCalculator/WelderCalculator/Helpers/DeLongHelpers/DeLongMicrophaseHelper.cs
+++ WelderCalculator/WelderCalculator/Helpers/DeLongHelpers/DeLongMicrophaseHelper.cs
@@ -2,6 +2,31 @@
 
 namespace WelderCalculator.Helpers.DeLongHelpers
 {
+
+    public enum DeLongMicrophase
+    {
+        A,
+        AM,
+        AMF,
+        AF,
+        Unknown
+    }
+
+    public enum DeLongFerriteQuantity
+    {
+        _0to2,
+        _2to4,
+        _4to6,
+        _6to8,
+        _8to10,
+        _10to12,
+        _12to14,
+        _14to16,
+        _16to18,
+        _18Plus,
+        Unknown
+    }
+
     public class DeLongMicrophaseHelper
     {
         private DeLongMicrophaseBorders _borders;
@@ -9,6 +34,46 @@ namespace WelderCalculator.Helpers.DeLongHelpers
         public DeLongMicrophaseHelper()
         {
             _borders = new DeLongMicrophaseBorders();
+        }
+
+        public DeLongFerriteQuantity GetFerriteQuantityForPoint(PointF point)
+        {
+            if (F0to2Contains(point))
+                return DeLongFerriteQuantity._0to2;
+            if (F2to4Contains(point))
+                return DeLongFerriteQuantity._2to4;
+            if (F4to6Contains(point))
+                return DeLongFerriteQuantity._4to6;
+            if (F6to8Contains(point))
+                return DeLongFerriteQuantity._6to8;
+            if (F8to10Contains(point))
+                return DeLongFerriteQuantity._8to10;
+            if (F10to12Contains(point))
+                return DeLongFerriteQuantity._10to12;
+            if (F12to14Contains(point))
+                return DeLongFerriteQuantity._12to14;
+            if (F14to16Contains(point))
+                return DeLongFerriteQuantity._14to16;
+            if (F16to18Contains(point))
+                return DeLongFerriteQuantity._16to18;
+            if (F18PlusContains(point))
+                return DeLongFerriteQuantity._18Plus;
+            else
+                return DeLongFerriteQuantity.Unknown;
+        }
+
+        public DeLongMicrophase GetMicrophaseForPoint(PointF point)
+        {
+            if (AMContains(point) && GetFerriteQuantityForPoint(point) == DeLongFerriteQuantity.Unknown)
+                return DeLongMicrophase.AM;
+            if (AContains(point))
+                return DeLongMicrophase.A;
+            if (AMContains(point) && GetFerriteQuantityForPoint(point) != DeLongFerriteQuantity.Unknown)
+                return DeLongMicrophase.AMF;
+            if (!(AMContains(point)) && GetFerriteQuantityForPoint(point) != DeLongFerriteQuantity.Unknown)
+                return DeLongMicrophase.AF;
+            else
+                return DeLongMicrophase.Unknown
         }
 
         public bool AMContains(PointF point)
