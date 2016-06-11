f1ff6dbaca6edafaf99ad8cf076c708040f7ce45
bartoszbaczek <bartosz.baczek@gmail.com>
2016-06-11 11:58:52 +0200

    deleted old code from PDFGenerator

--- WelderCalculator/WelderCalculator/PDFUtilities/PDFGenerator.cs
+++ WelderCalculator/WelderCalculator/PDFUtilities/PDFGenerator.cs
@@ -1,38 +1,10 @@
-﻿using System.Diagnostics;
-using PdfSharp;
-using PdfSharp.Drawing;
-using PdfSharp.Pdf;
-using PdfSharp.Pdf.IO;
-
-
-
-
+﻿
 namespace WelderCalculator.PDFUtilities
 {
     public class PDFGenerator
     {
         public PDFGenerator()
         {
-            // Create a new PDF document
-            PdfDocument document = new PdfDocument();
-            document.Info.Title = "Created with PDFsharp";
-            // Create an empty page
-            PdfPage page = document.AddPage();
-            // Get an XGraphics object for drawing
-            XGraphics gfx = XGraphics.FromPdfPage(page);
-            // Create a font
-            XFont font = new XFont("Verdana", 20, XFontStyle.BoldItalic);
-            
-            
-            // Draw the text
-            gfx.DrawString("Hello, World!", font, XBrushes.Black,
-            new XRect(0, 0, page.Width, page.Height),
-            XStringFormats.Center);
-            // Save the document...
-            const string filename = "HelloWorld.pdf";
-            document.Save(filename);
-            // ...and start a viewer.
-            Process.Start(filename);
         }
 
     }
